ae1cdfd8c52b45276960ba23b1ffffa886d85af9
Niki Skaarup <nikiskaarup@hotmail.com>
2017-12-19 13:17:47 +0100

    init

---
+++ fs/init.js
@@ -0,0 +1,67 @@
+load('api_config.js');
+load('api_gpio.js');
+load('api_net.js');
+load('api_sys.js');
+load('api_timer.js');
+load('api_http.js');
+
+let led = Cfg.get('pins.led');
+let url = Cfg.get('url');
+let device_identifier = Cfg.get('device.id');
+
+print('LED GPIO:', led);
+
+let getInfo = function () {
+  return JSON.stringify({
+    total_ram: Sys.total_ram(),
+    free_ram: Sys.free_ram()
+  });
+};
+
+function query(data) {
+  print(JSON.stringify(data));
+  HTTP.query({
+    url: url,
+    headers: { 'Content-type': 'application/json' },
+    data: data,
+    success: function (body, full_http_msg) { print(body); },
+    error: function (err) { print(err); }
+  });
+}
+
+function getData() {
+  return {
+    "device_identifier": device_identifier,
+    "data": {
+      "relay_state": false,
+      "light_level": 0,
+      "temperature": 0.0,
+      "milis": Sys.uptime()
+    }
+  };
+}
+
+GPIO.set_mode(led, GPIO.MODE_OUTPUT);
+Timer.set(2000, true /* repeat */, function () {
+  let value = GPIO.toggle(led);
+  print(value ? 'Tick' : 'Tock', 'uptime:', Sys.uptime(), getInfo());
+}, null);
+
+Timer.set(10 * 1000, true /* repeat */, function () {
+  query(getData());
+}, null);
+
+// Monitor network connectivity.
+Net.setStatusEventHandler(function (ev, arg) {
+  let evs = '???';
+  if (ev === Net.STATUS_DISCONNECTED) {
+    evs = 'DISCONNECTED';
+  } else if (ev === Net.STATUS_CONNECTING) {
+    evs = 'CONNECTING';
+  } else if (ev === Net.STATUS_CONNECTED) {
+    evs = 'CONNECTED';
+  } else if (ev === Net.STATUS_GOT_IP) {
+    evs = 'GOT_IP';
+  }
+  print('== Net event:', ev, evs);
+}, null);
