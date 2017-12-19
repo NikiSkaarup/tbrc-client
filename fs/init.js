15e0268a65e90cd48f48bc972b022e4366309961
Niki W. Skaarup <nikiskaarup@hotmail.com>
2017-12-19 21:31:56 +0100

    added temp sensor

--- fs/init.js
+++ fs/init.js
@@ -4,10 +4,19 @@ load('api_net.js');
 load('api_sys.js');
 load('api_timer.js');
 load('api_http.js');
+load('api_arduino_onewire.js');
+load('api_arduino_dallas_temp.js');
 
 let led = Cfg.get('pins.led');
 let url = Cfg.get('url');
 let device_identifier = Cfg.get('device.id');
+
+let tempSensor = Cfg.get('pins.tempsensor');
+let ow = OneWire.create(tempSensor);
+let dt = DallasTemperature.create(ow);
+dt.begin();
+let n = 0;
+let sens = [];
 
 print('LED GPIO:', led);
 
@@ -30,19 +39,35 @@ function query(data) {
 }
 
 function getData() {
+  let temp = DallasTemperature.DEVICE_DISCONNECTED_C;
+  if (n === 0) {
+    n = dt.getDeviceCount();
+    print('Sensors found:', n, 'pin:', tempSensor);
+
+    for (let i = 0; i < n; i++) {
+      sens[i] = '01234567';
+      if (dt.getAddress(sens[i], i) === 1) {
+        print('Sensor#', i, 'address:', dt.toHexStr(sens[i]));
+      }
+    }
+  } else if (n >= 1) {
+    dt.requestTemperatures();
+    temp = dt.getTempC(sens[0]);
+  }
+
   return {
     "device_identifier": device_identifier,
     "data": {
       "relay_state": false,
       "light_level": 0,
-      "temperature": 0.0,
+      "temperature": temp,
       "milis": Sys.uptime()
     }
   };
 }
 
 GPIO.set_mode(led, GPIO.MODE_OUTPUT);
-Timer.set(2000, true /* repeat */, function () {
+Timer.set(2 * 1000, true /* repeat */, function () {
   let value = GPIO.toggle(led);
   print(value ? 'Tick' : 'Tock', 'uptime:', Sys.uptime(), getInfo());
 }, null);
